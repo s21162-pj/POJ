67a4f3746a5f2ff9e1cd82c1cda4f0148a943cae
s21162-pj <56191739+s21162-pj@users.noreply.github.com>
2020-03-09 23:38:00 +0100

    Create Zad2.java

---
+++ zjazd 1/Zad2.java
@@ -0,0 +1,46 @@
+package com.company;
+/* Zadanie 2. - Kalkulator BMI
+   Autor: Robert Brzoskowski
+   Index: s21162
+ */
+import java.util.Scanner;
+
+public class Zad2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("To jest kalkulator BMI, podaj swój wzrost w cm: ");
+        Scanner input = new Scanner(System.in);
+        double  wzrost = input.nextDouble();
+        System.out.println("Podaj swoją wagę: ");
+        double waga = input.nextDouble();
+
+        double bmi = ((10000*waga)/(wzrost*wzrost));
+
+        System.out.println("Twoje BMi wynosi: " +bmi);
+        if (bmi>40){
+            System.out.println("otyłość skrajna");
+        }
+        else if (bmi>35){
+            System.out.println("II stopień otyłoście");
+        }
+        else if (bmi>30){
+            System.out.println("I stopień otyłości");
+        }
+        else if (bmi>25){
+            System.out.println("nadwaga");
+        }
+        else if (bmi>18){
+            System.out.println("wartość prawidłowa");
+        }
+        else if (bmi>17){
+            System.out.println("niedowaga");
+        }
+        else if (bmi>16){
+            System.out.println("wychudzenie");
+        }
+        else if (bmi<16){
+            System.out.println("wygłodzenie");
+        }
+    }
+}
