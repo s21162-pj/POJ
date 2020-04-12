5c5574c334564caf2e75c5c38a9ea739705ab5fe
s21162-pj <56191739+s21162-pj@users.noreply.github.com>
2020-04-12 21:29:45 +0200

    Create Shape.java

---
+++ Zjazd 4/Zad 23/Shape.java
@@ -0,0 +1,33 @@
+/* Zadanie 23. - Circle, Rectangle, Shape, Square
+   Autor: Robert Brzoskowski
+   Index: s21162
+ */
+
+package com.company;
+public class Shape {
+    private String color;
+    private boolean filled;
+    public Shape() {
+        color = "red";
+        filled = true;
+    }
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public boolean isFilled() {
+        return filled;
+    }
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    public String toString() {
+        return "Shape[" + "color=" + color + "," + ", filled=" + filled + "]";
+    }
+}
