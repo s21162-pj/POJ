de71e74551906d42e0498bb42e51ef90ac41aa21
s21162-pj <56191739+s21162-pj@users.noreply.github.com>
2020-04-12 21:50:04 +0200

    Create Test.java

---
+++ Zjazd 4/Zad 24/Test.java
@@ -0,0 +1,15 @@
+/* Zadanie 24. - Armstrong Number Checker
+   Autor: Robert Brzoskowski
+   Index: s21162
+ */
+
+package com.company;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Armstrong.isArmstrong(123));
+        System.out.println(Armstrong.isArmstrong(153));
+
+    }
+}
